993009944830f682d7e87040b2cbd73668f18267
Dontalashanmukhasai <98035268+Dontalashanmukhasai@users.noreply.github.com>
2023-08-02 20:54:39 +0530

    Add files via upload

---
+++ string_functions.py
@@ -0,0 +1,9 @@
+mystr = "abcdefghijklmnopqrstuvwxyz"
+print(len(mystr))
+print(mystr.endswith("ghi"))
+print(mystr.startswith("abc"))
+print(mystr.count('a'))
+mystr1 = "my name is shannu"
+print(mystr1.capitalize())
+print(mystr1.find("shannu"))
+print(mystr1.replace("shannu","vasu"))
