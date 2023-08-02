5e900f6fcad98d4f7829531eb632206644aba418
Dontalashanmukhasai <98035268+Dontalashanmukhasai@users.noreply.github.com>
2023-08-02 17:31:43 +0530

    Add files via upload

---
+++ operations.py
@@ -0,0 +1,35 @@
+a = 45
+b = 20
+#Arthmetic Operations in python:
+print("a + b=", a+b)
+print("a - b=", a-b)
+print("a * b=", a*b)
+print("a / b=", a/b)
+
+k = int(input("Enter the first number:"))
+R = int(input("Enter the second number:"))
+print("The sum of the two numbers is:",k+R)
+
+#Assignment operations in python:
+a = 20
+b = 4
+print(a,b)
+a+=5
+b+=12
+print(a,b)
+a-=3
+b-=4
+print(a,b)
+
+#comparison operator in python:
+a = int(input("enter the number:"))
+b = int(input("enter the nnumber:"))
+if(a==b):
+    print("a is equal to b")
+else:
+    print("a is not equal to b")
+if(a>b):
+    print("a is greater than b")
+else:
+    print("a is less than b")
+    
